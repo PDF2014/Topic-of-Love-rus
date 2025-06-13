908771d6c13c63e17ce7b55edd5ca2ef8511faf5
Mian <docgoosenn@gmail.com>
2025-06-12 20:18:07 -0700

    Removed scar of incest (incest has no gameplay value to it)

--- Mian/CustomAssets/Traits/CultureTraits.cs
+++ Mian/CustomAssets/Traits/CultureTraits.cs
@@ -42,7 +42,7 @@ namespace Topic_of_Love.Mian.CustomAssets.Traits
                 can_be_given = true
             }, List.Of("flower_bud", "garl")).addOpposite("homophobic");
 
-            // incest was a bug in the original game so let's not actually make it a feature cuz uh.. for obvious reasons
+            // incest doesnt really have any gameplay value to it :shrug:
             // Add(new CultureTrait
             // {
             //     id = "incest",
@@ -52,6 +52,16 @@ namespace Topic_of_Love.Mian.CustomAssets.Traits
             //     can_be_in_book = true,
             //     can_be_removed = true,
             //     can_be_given = true
+            // }, List.Of("orc", "demon"), List.Of("biome_infernal", "biome_corrupted"));
+            //
+            // Add(new CultureTrait
+            // {
+            //     id = "scar_of_incest",
+            //     group_id = "miscellaneous",
+            //     rarity = Rarity.R1_Rare,
+            //     can_be_given = true,
+            //     can_be_in_book = false,
+            //     can_be_removed = true,
             // }, List.Of("orc", "demon"), List.Of("biome_infernal", "biome_corrupted"));
             
             Add(new CultureTrait
@@ -86,16 +96,6 @@ namespace Topic_of_Love.Mian.CustomAssets.Traits
                 can_be_removed = true,
                 can_be_given = true
             }, List.Of("elf"), List.Of("biome_maple"));
-            
-            Add(new CultureTrait
-            {
-                id = "scar_of_incest",
-                group_id = "miscellaneous",
-                rarity = Rarity.R1_Rare,
-                can_be_given = true,
-                can_be_in_book = false,
-                can_be_removed = true,
-            }, List.Of("orc", "demon"), List.Of("biome_infernal", "biome_corrupted"));
             Finish();
         }
     }
