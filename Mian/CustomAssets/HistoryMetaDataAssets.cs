0d1ced6cb03f0b1b781835a52de30a81b819a5dd
Mian <docgoosenn@gmail.com>
2025-06-22 18:01:08 -0700

    Statistics can now be added via one method omg im ao gooded?? ill pr this into NML when i can

--- Mian/CustomAssets/HistoryMetaDataAssets.cs
+++ Mian/CustomAssets/HistoryMetaDataAssets.cs
@@ -5,6 +5,7 @@ using System.Reflection;
 using System.Reflection.Emit;
 using db;
 using db.tables;
+using NCMS.Extensions;
 using NeoModLoader.General.Game.extensions;
 using SQLite;
 using UnityEngine;
@@ -29,221 +30,93 @@ public class HistoryMetaDataAssets
     private static readonly ModuleBuilder DynamicModule =  
         AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("DynamicTypesAssembly"), AssemblyBuilderAccess.Run)
         .DefineDynamicModule("MainModule");
-    // public class KingdomTableExtras : KingdomTable
-    // {
-    //     [Column("lonely")] public long? lonely { get; set; }
-    // }
-    //
-    // public class KingdomYearly1 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly5 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly10 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly100 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly500 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly1000 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly5000 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly10000 : KingdomTableExtras
-    // {
-    // }
-    //
-    // public class KingdomYearly50 : KingdomTableExtras
-    // {
-    // }
+
+    private static readonly Dictionary<string, Dictionary<string, Func<NanoObject, long?>>> VanillaCollectors = new()
+    {
+        ["kingdom"] =
+        new (){
+            ["population"] = obj => ((Kingdom) obj).countUnits(),
+            ["adults"] = obj => ((Kingdom) obj).countAdults(),
+            ["children"] = obj => ((Kingdom) obj).countChildren(),
+            ["boats"] = obj => ((Kingdom) obj).countBoats(),
+            ["army"] = obj => ((Kingdom) obj).countTotalWarriors(),
+            ["sick"] = obj => ((Kingdom) obj).countSick(),
+            ["hungry"] = obj => ((Kingdom) obj).countHungry(),
+            ["starving"] = obj => ((Kingdom) obj).countStarving(),
+            ["happy"] = obj => ((Kingdom)obj).countHappyUnits(),
+            ["deaths"] = obj => ((Kingdom)obj).getTotalDeaths(),
+            ["births"] = obj => ((Kingdom)obj).getTotalBirths(),
+            ["kills"] = obj => ((Kingdom)obj).getTotalKills(),
+            ["joined"] = obj => ((Kingdom)obj).getTotalJoined(),
+            ["left"] = obj => ((Kingdom)obj).getTotalLeft(),
+            ["moved"] = obj => ((Kingdom)obj).getTotalMoved(),
+            ["migrated"] = obj => ((Kingdom)obj).getTotalMigrated(),
+            ["territory"] = obj => ((Kingdom)obj).countZones(),
+            ["buildings"] = obj => ((Kingdom)obj).countBuildings(),
+            ["homeless"] = obj => ((Kingdom)obj).countHomeless(),
+            ["housed"] = obj => ((Kingdom)obj).countHoused(),
+            ["food"] = obj => ((Kingdom)obj).countTotalFood(),
+            ["families"] = obj => ((Kingdom)obj).countFamilies(),
+            ["males"] = obj => ((Kingdom)obj).countMales(),
+            ["females"] = obj => ((Kingdom)obj).countFemales(),
+            ["cities"] = obj => ((Kingdom)obj).countCities(),
+            ["renown"] = obj => ((Kingdom)obj).getRenown(),
+            ["money"] = obj => ((Kingdom)obj).countTotalMoney(),
+            ["deaths_eaten"] = obj => ((Kingdom)obj).getDeaths(AttackType.Eaten),
+            ["deaths_hunger"] = obj => ((Kingdom)obj).getDeaths(AttackType.Starvation),
+            ["deaths_natural"] = obj => ((Kingdom)obj).getDeaths(AttackType.Age),
+            ["deaths_plague"] = obj => ((Kingdom)obj).getDeaths(AttackType.Plague),
+            ["deaths_poison"] = obj => ((Kingdom)obj).getDeaths(AttackType.Poison),
+            ["deaths_infection"] = obj => ((Kingdom)obj).getDeaths(AttackType.Infection),
+            ["deaths_tumor"] = obj => ((Kingdom)obj).getDeaths(AttackType.Tumor),
+            ["deaths_acid"] = obj => ((Kingdom)obj).getDeaths(AttackType.Acid),
+            ["deaths_fire"] = obj => ((Kingdom)obj).getDeaths(AttackType.Fire),
+            ["deaths_divine"] = obj => ((Kingdom)obj).getDeaths(AttackType.Divine),
+            ["deaths_weapon"] = obj => ((Kingdom)obj).getDeaths(AttackType.Weapon),
+            ["deaths_gravity"] = obj => ((Kingdom)obj).getDeaths(AttackType.Gravity),
+            ["deaths_drowning"] = obj => ((Kingdom)obj).getDeaths(AttackType.Drowning),
+            ["deaths_water"] = obj => ((Kingdom)obj).getDeaths(AttackType.Water),
+            ["deaths_explosion"] = obj => ((Kingdom)obj).getDeaths(AttackType.Explosion),
+            ["deaths_other"] = obj => ((Kingdom)obj).getDeaths(AttackType.Other),
+            ["metamorphosis"] = obj => ((Kingdom)obj).getDeaths(AttackType.Metamorphosis),
+            ["evolutions"] = obj => ((Kingdom)obj).getEvolutions(),
+        }
+    };
 
     public static void Init()
     {
         
-        AddFields("kingdom", nano =>
+        AddStatisticsToMetaAsset("kingdom", new(){"lonely"}, nano =>
         {
             var kingdom = (Kingdom) nano;
             return new(){
-                ["lonely"] = ((Kingdom)nano).countLonely(),
-                ["population"] = new long?((long)kingdom.countUnits()),
-                adults = new long?((long)kingdom.countAdults()),
-                children = new long?((long)kingdom.countChildren()),
-                boats = new long?((long)kingdom.countBoats()),
-                army = new long?((long)kingdom.countTotalWarriors()),
-                sick = new long?((long)kingdom.countSick()),
-                hungry = new long?((long)kingdom.countHungry()),
-                starving = new long?((long)kingdom.countStarving()),
-                happy = new long?((long)kingdom.countHappyUnits()),
-                deaths = new long?(kingdom.getTotalDeaths()),
-                births = new long?(kingdom.getTotalBirths()),
-                kills = new long?(kingdom.getTotalKills()),
-                joined = new long?(kingdom.getTotalJoined()),
-                left = new long?(kingdom.getTotalLeft()),
-                moved = new long?(kingdom.getTotalMoved()),
-                migrated = new long?(kingdom.getTotalMigrated()),
-                territory = new long?((long)kingdom.countZones()),
-                buildings = new long?((long)kingdom.countBuildings()),
-                homeless = new long?((long)kingdom.countHomeless()),
-                housed = new long?((long)kingdom.countHoused()),
-                food = new long?((long)kingdom.countTotalFood()),
-                families = new long?((long)kingdom.countFamilies()),
-                males = new long?((long)kingdom.countMales()),
-                females = new long?((long)kingdom.countFemales()),
-                cities = new long?((long)kingdom.countCities()),
-                renown = new long?((long)kingdom.getRenown()),
-                money = new long?((long)kingdom.countTotalMoney()),
-                deaths_eaten = new long?(kingdom.getDeaths(AttackType.Eaten)),
-                deaths_hunger = new long?(kingdom.getDeaths(AttackType.Starvation)),
-                deaths_natural = new long?(kingdom.getDeaths(AttackType.Age)),
-                deaths_plague = new long?(kingdom.getDeaths(AttackType.Plague)),
-                deaths_poison = new long?(kingdom.getDeaths(AttackType.Poison)),
-                deaths_infection = new long?(kingdom.getDeaths(AttackType.Infection)),
-                deaths_tumor = new long?(kingdom.getDeaths(AttackType.Tumor)),
-                deaths_acid = new long?(kingdom.getDeaths(AttackType.Acid)),
-                deaths_fire = new long?(kingdom.getDeaths(AttackType.Fire)),
-                deaths_divine = new long?(kingdom.getDeaths(AttackType.Divine)),
-                deaths_weapon = new long?(kingdom.getDeaths(AttackType.Weapon)),
-                deaths_gravity = new long?(kingdom.getDeaths(AttackType.Gravity)),
-                deaths_drowning = new long?(kingdom.getDeaths(AttackType.Drowning)),
-                deaths_water = new long?(kingdom.getDeaths(AttackType.Water)),
-                deaths_explosion = new long?(kingdom.getDeaths(AttackType.Explosion)),
-                deaths_other = new long?(kingdom.getDeaths(AttackType.Other)),
-                metamorphosis = new long?(kingdom.getDeaths(AttackType.Metamorphosis)),
-                evolutions = new long?(kingdom.getEvolutions())
+                ["lonely"] = kingdom.countLonely(),
             };
         });
-        
-        // var kingdom = AssetManager.history_meta_data_library.get("kingdom");
-        // kingdom.table_type = typeof(KingdomTableExtras);
-        // kingdom.table_types = new Dictionary<HistoryInterval, Type>()
-        // {
-        //     {
-        //         HistoryInterval.Yearly1,
-        //         typeof(KingdomYearly1)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly5,
-        //         typeof(KingdomYearly5)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly10,
-        //         typeof(KingdomYearly10)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly50,
-        //         typeof(KingdomYearly50)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly100,
-        //         typeof(KingdomYearly100)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly500,
-        //         typeof(KingdomYearly500)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly1000,
-        //         typeof(KingdomYearly1000)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly5000,
-        //         typeof(KingdomYearly5000)
-        //     },
-        //     {
-        //         HistoryInterval.Yearly10000,
-        //         typeof(KingdomYearly10000)
-        //     },
-        // };
-        // kingdom.collector = (HistoryDataCollector)(pNanoObject =>
-        // {
-        //     Kingdom kingdom = (Kingdom)pNanoObject;
-        //     return (HistoryTable)new KingdomYearly1()
-        //     {
-        //         id = kingdom.getID(),
-        //         lonely = new long?((long)kingdom.countLonely()),
-        //         population = new long?((long)kingdom.countUnits()),
-        //         adults = new long?((long)kingdom.countAdults()),
-        //         children = new long?((long)kingdom.countChildren()),
-        //         boats = new long?((long)kingdom.countBoats()),
-        //         army = new long?((long)kingdom.countTotalWarriors()),
-        //         sick = new long?((long)kingdom.countSick()),
-        //         hungry = new long?((long)kingdom.countHungry()),
-        //         starving = new long?((long)kingdom.countStarving()),
-        //         happy = new long?((long)kingdom.countHappyUnits()),
-        //         deaths = new long?(kingdom.getTotalDeaths()),
-        //         births = new long?(kingdom.getTotalBirths()),
-        //         kills = new long?(kingdom.getTotalKills()),
-        //         joined = new long?(kingdom.getTotalJoined()),
-        //         left = new long?(kingdom.getTotalLeft()),
-        //         moved = new long?(kingdom.getTotalMoved()),
-        //         migrated = new long?(kingdom.getTotalMigrated()),
-        //         territory = new long?((long)kingdom.countZones()),
-        //         buildings = new long?((long)kingdom.countBuildings()),
-        //         homeless = new long?((long)kingdom.countHomeless()),
-        //         housed = new long?((long)kingdom.countHoused()),
-        //         food = new long?((long)kingdom.countTotalFood()),
-        //         families = new long?((long)kingdom.countFamilies()),
-        //         males = new long?((long)kingdom.countMales()),
-        //         females = new long?((long)kingdom.countFemales()),
-        //         cities = new long?((long)kingdom.countCities()),
-        //         renown = new long?((long)kingdom.getRenown()),
-        //         money = new long?((long)kingdom.countTotalMoney()),
-        //         deaths_eaten = new long?(kingdom.getDeaths(AttackType.Eaten)),
-        //         deaths_hunger = new long?(kingdom.getDeaths(AttackType.Starvation)),
-        //         deaths_natural = new long?(kingdom.getDeaths(AttackType.Age)),
-        //         deaths_plague = new long?(kingdom.getDeaths(AttackType.Plague)),
-        //         deaths_poison = new long?(kingdom.getDeaths(AttackType.Poison)),
-        //         deaths_infection = new long?(kingdom.getDeaths(AttackType.Infection)),
-        //         deaths_tumor = new long?(kingdom.getDeaths(AttackType.Tumor)),
-        //         deaths_acid = new long?(kingdom.getDeaths(AttackType.Acid)),
-        //         deaths_fire = new long?(kingdom.getDeaths(AttackType.Fire)),
-        //         deaths_divine = new long?(kingdom.getDeaths(AttackType.Divine)),
-        //         deaths_weapon = new long?(kingdom.getDeaths(AttackType.Weapon)),
-        //         deaths_gravity = new long?(kingdom.getDeaths(AttackType.Gravity)),
-        //         deaths_drowning = new long?(kingdom.getDeaths(AttackType.Drowning)),
-        //         deaths_water = new long?(kingdom.getDeaths(AttackType.Water)),
-        //         deaths_explosion = new long?(kingdom.getDeaths(AttackType.Explosion)),
-        //         deaths_other = new long?(kingdom.getDeaths(AttackType.Other)),
-        //         metamorphosis = new long?(kingdom.getDeaths(AttackType.Metamorphosis)),
-        //         evolutions = new long?(kingdom.getEvolutions())
-        //     };
-        // });
     }
 
-    // gotta add known collectors
-    // define table type and table types
-    public static void AddFields(string id, string[] keys, Func<NanoObject, Dictionary<string, long>> collector)
+    public static void AddStatisticsToMetaAsset(string id, List<string> keys, Func<NanoObject, Dictionary<string, long?>> collector)
     {
+
         var capitalized = id.Substring(0, 1).ToUpper() + id.Substring(1); 
         var asset = AssetManager.history_meta_data_library.get(id);
 
         var tableExtras = DynamicModule.DefineType(capitalized + "TableExtras",
             TypeAttributes.Public | TypeAttributes.Class,
             asset.table_type);
-        
+        // SQL's Column attribute
         var attrConstructor = typeof(ColumnAttribute).GetConstructor(new[]{typeof(string)});
 
-        foreach (var key in keys)
+        keys.AddRange(VanillaCollectors[id].Keys);
+        
+        foreach (var statistic in keys)
         {
-            var field = tableExtras.DefineField("_" + key, typeof(long?), FieldAttributes.Private);
-            var propBuilder = tableExtras.DefineProperty(key, PropertyAttributes.HasDefault, typeof(long?), null);
-            var attrBuilder = new CustomAttributeBuilder(attrConstructor, new object[] { key });
+            var field = tableExtras.DefineField("_" + statistic, typeof(long?), FieldAttributes.Private);
+            var propBuilder = tableExtras.DefineProperty(statistic, PropertyAttributes.HasDefault, typeof(long?), null);
+            var attrBuilder = new CustomAttributeBuilder(attrConstructor, new object[] { statistic });
             field.SetCustomAttribute(attrBuilder);
 
-            var getMethod = tableExtras.DefineMethod("get_" + key,
+            var getMethod = tableExtras.DefineMethod("get_" + statistic,
                 MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
                 typeof(long?),
                 Type.EmptyTypes);
@@ -253,11 +126,11 @@ public class HistoryMetaDataAssets
             generatorForGet.Emit(OpCodes.Ldfld, field);
             generatorForGet.Emit(OpCodes.Ret);
 
-            var setMethod = tableExtras.DefineMethod("set_" + key,
+            var setMethod = tableExtras.DefineMethod("set_" + statistic,
                 MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig,
                 null,
                 new[]{typeof(long?)});
-            var generatorForSet = getMethod.GetILGenerator();
+            var generatorForSet = setMethod.GetILGenerator();
             generatorForSet.Emit(OpCodes.Ldarg_0);
             generatorForSet.Emit(OpCodes.Ldarg_1);
             generatorForSet.Emit(OpCodes.Stfld, field);
@@ -266,6 +139,7 @@ public class HistoryMetaDataAssets
             propBuilder.SetGetMethod(getMethod);
             propBuilder.SetSetMethod(setMethod);
         }
+        
         var tableExtrasType = tableExtras.CreateType();
         
         asset.table_type = tableExtrasType;
@@ -279,12 +153,17 @@ public class HistoryMetaDataAssets
         asset.collector = nano =>
         {
             var dictionary = collector.Invoke(nano);
-            var instance = Activator.CreateInstance(tableExtrasType) as HistoryTable;
+            dictionary.AddRange(VanillaCollectors[id].ToDictionary(pair => pair.Key, pair => pair.Value.Invoke(nano)));
+            
+            var instance = Activator.CreateInstance(asset.table_types[HistoryInterval.Yearly1]) as HistoryTable;
             
             foreach (var pair in dictionary)
             {
                 tableExtrasType.GetProperty(pair.Key).SetValue(instance, pair.Value);
             }
+
+            instance.id = nano.getID();
+            return instance;
         };
     }
 }
