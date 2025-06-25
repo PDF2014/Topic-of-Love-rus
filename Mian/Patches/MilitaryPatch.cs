d54c502a2054447e1c429882e0bb5fac88ac6a8b
Mian <docgoosenn@gmail.com>
2025-06-24 23:47:46 -0700

    Homophobic/Heterophobic cultures prevent actors with specific orientations from getting into the military

---
+++ Mian/Patches/MilitaryPatch.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using Topic_of_Love.Mian.CustomAssets.Custom;
+
+namespace Topic_of_Love.Mian.Patches;
+
+[HarmonyPatch(typeof(City))]
+public class MilitaryPatch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(City.checkCanMakeWarrior))]
+    public static bool CanMakeWarrior(City __instance, Actor pActor, ref bool __result)
+    {
+        if (__instance.hasCulture())
+        {
+            if (__instance.culture.hasTrait("homophobic") && Orientation.IsAHomo(pActor))
+            {
+                __result = false;
+                return false;
+            }
+            if (__instance.culture.hasTrait("heterophobic") && Orientation.IsAHetero(pActor))
+            {
+                __result = false;
+                return false;
+            }
+        }
+        return true;
+    }
+}
